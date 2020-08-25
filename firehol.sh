a803a13d821d3097a25508ff4b95c7f85c61f5e5
Poli <raimundophilippe@gmail.com>
2020-08-25 17:50:52 +0200

    Update firehol.sh

--- firehol.sh
+++ firehol.sh
@@ -10,6 +10,10 @@
 	echo "*/13 * * * * root update-ipsets -s >/dev/null 2>&1" >> /tmp/tmpcron
 	crontab /tmp/tmpcron
 	rm /tmp/tmpcron
-  
+  	
+	update-rc.d firehol defaults
+	firehol start
+	
   echo "firehol has been installed and the crontab is running every 13 minutes" 
+  echo "We recommand you to close this session and to open a new one to prevent issues with the backspace"
   echo "Thanks for using it, by Poli Systems"
