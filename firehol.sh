ded70164d4536df173f7b855d505cf977bcb38cf
Poli <raimundophilippe@gmail.com>
2020-08-25 19:20:15 +0200

    Update firehol.sh

--- firehol.sh
+++ firehol.sh
@@ -3,7 +3,7 @@
   
   apt install firehol firehol-tools -y
   echo "        server all accept" >> /etc/firehol/firehol.conf
-	update-ipsets enable feodo palevo sslbl zeus_badips dshield spamhaus_drop spamhaus_edrop openbl blocklist_de bi_ssh-ddos_0_1d bi_ssh-ddos_2_30d bi_ssh_0_1d bi_ssh_1_7d bi_ssh_2_30d bi_sshd_0_1d bi_wordpress_0_1d bi_wordpress_1_7d bi_wordpress_2_30d firehol_level1 firehol_level2 firehol_level3 dshield_top_1000 bruteforceblocker malc0de ransomware_rw greensnow myip fullbogons
+	update-ipsets enable  dshield spamhaus_drop spamhaus_edrop blocklist_de firehol_level1 firehol_level2 firehol_level3 dshield_top_1000 bruteforceblocker malc0de greensnow myip fullbogons
 	update-ipsets -s
 	
 	crontab -l > /tmp/tmpcron
